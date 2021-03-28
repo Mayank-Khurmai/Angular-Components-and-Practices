109609f486fe840d6fc6742d9b9aa6d1aea88030
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-03-28 18:41:41 +0530

    Components

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionComponent implements OnInit {
  
-  public sum: number = 0;
+  public sum: number = 1;
   constructor() { }
 
   ngOnInit(): void {
